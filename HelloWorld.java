6b606b93bb7da9c458b0a8165c0210fc05e71307
ss0-rgb <dpsrushti20@gmail.com>
2025-11-30 14:58:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example;
+
 public class HelloWorld {
-public static void main(String[] args) {
-System.out.println(&quot;Hello, DevOps CI/CD using GitHub Actions!&quot;);
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, DevOps CI/CD using GitHub Actions!");
+    }
 }
